0028c397d1504e6e8b7b187955ad6a5480dee61d
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:03:24 +0800

    aaa

--- 20220307/Program.cs
+++ 20220307/Program.cs
@@ -3,10 +3,20 @@
 namespace _20220307
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        double meal_cost = 100;
+
+        int tip_percent = 15;
+
+        int tax_percent = 8;
+
+        double money;
+
+        money = meal_cost + tip_percent + tax_percent;
+
+        Console.WriteLine(money);
     }
+}
 }
